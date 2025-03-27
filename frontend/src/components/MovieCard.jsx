b7c9d0193e0fcce29cc4235f7474d8fd48aff478
Nitesh Kumar <niteshkumaro7@hotmail.com>
2025-03-27 10:48:17 +0530

    changes

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -3,22 +3,19 @@ import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
   return (
-    <div className="p-4 border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out bg-white w-full sm:w-64">
-      
+    <div className="p-4 border-s-neutral-100  rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out w-full sm:w-64">
       <img
         src={movie.Poster !== "N/A" ? movie.Poster : "/placeholder.png"}
         alt={movie.Title}
         className="w-full h-72 object-cover rounded-md"
       />
 
-  
       <div className="mt-4 space-y-2">
         <h3 className="text-lg font-semibold text-gray-800">{movie.Title}</h3>
         <p className="text-sm text-gray-600">📅 Year: {movie.Year}</p>
         <p className="text-sm text-gray-500">🎬 IMDB ID: {movie.imdbID}</p>
       </div>
 
- 
       <Link to={`/movies/${movie.imdbID}`} className="block mt-4">
         <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-200">
           🎥 View Details
